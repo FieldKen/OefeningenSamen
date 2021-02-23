977742dd506f54e593a2cfadbe1487bb70dcf44f
Ken <fieldken@hotmail.com>
2021-02-23 13:00:57 +0100

    Add project files.

---
+++ Oefening02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Prijs van een drankje: ");
+            double prijs = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Aantal drankjes: ");
+            int aantal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Prijs zonder btw: {prijs * aantal}");
+            Console.WriteLine($"Prijs met 21% btw: {(prijs * aantal * 1.21).ToString("0.00")}");
+            Console.WriteLine($"Prijs met 21% btw: {Math.Round(prijs * aantal * 1.21,2)}");
+
+            Console.ReadLine();
+        }
+    }
+}
